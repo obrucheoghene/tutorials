d1ecbf0898e10bb53556bda31dd6da0ae7251b00
wilfredcloud <wilfredcloudspace@gmail.com>
2023-02-27 21:49:24 +0100

    Add login component

---
+++ react_redux/src/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="text" name="" id="" className="input" />
+
+      <div>
+        <button className="btn-login">LOGIN</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
